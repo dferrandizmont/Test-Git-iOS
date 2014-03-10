2945d166bca680d1210b7b547ff1bfe79a729512
Dani Ferrandiz Mont <dferrandizmont@gmail.com>
2014-03-10 14:48:38 +0100

    added a test class

---
+++ DFMTest.h
@@ -0,0 +1,13 @@
+//
+//  DFMTest.h
+//  Git Test
+//
+//  Created by Daniels Ferràndiz Mont on 10/03/14.
+//  Copyright (c) 2014 Daniel Ferràndiz Mont. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DFMTest : NSObject
+
+@end
---
+++ DFMTest.m
@@ -0,0 +1,13 @@
+//
+//  DFMTest.m
+//  Git Test
+//
+//  Created by Daniels Ferràndiz Mont on 10/03/14.
+//  Copyright (c) 2014 Daniel Ferràndiz Mont. All rights reserved.
+//
+
+#import "DFMTest.h"
+
+@implementation DFMTest
+
+@end
